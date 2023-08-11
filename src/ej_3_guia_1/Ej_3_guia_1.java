467e2ace2418b145dd7e28fcbda12f83f9a9c5d2
GMTUDS <142052001+GMTUDS@users.noreply.github.com>
2023-08-11 17:19:33 -0300

    Initial commit

---
+++ src/ej_3_guia_1/Ej_3_guia_1.java
@@ -0,0 +1,23 @@
+
+package ej_3_guia_1;
+
+public class Ej_3_guia_1 {
+
+  
+    public static void main(String[] args) {
+     Jugador j1 =new Jugador();
+     j1.setNombre("juan");
+     j1.setEdad(22);
+     j1.setPuntaje(9);
+     j1.setNacionalidad("argentino");
+     
+        System.out.println("nombre "+j1.getNombre());
+        System.out.println("edad "+j1.getEdad());
+        System.out.println("puntaje "+j1.getPuntaje());
+        System.out.println("nacionaldiad "+j1.getNacionalidad());
+        
+     
+        
+    }
+    
+}
